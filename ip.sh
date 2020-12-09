5e316fb0d7520b4f04b49a0ca6184d48f18166af
luis2046200 <62407028+luis2046200@users.noreply.github.com>
2020-12-08 21:44:30 -0500

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-#Pregunta si o no
-echo -n "Desea saber su Ping (y/n)? "
-read answer
-clear
-#Buscador del ip local
-printf "Tu IP:"
-hostname -I | cut -d' ' -f1 
-# if echo "$answer" | grep -iq "^y" ;then
+#! / bin / bash
+# Pregunta si o no
+echo -n "¿ Desea saber su Ping (y / n)? "
+leer respuesta
+claro
+# Buscador del ip local
+printf  " Tu IP: "
+nombre de host -i | cortar -d '  ' -f1
+# if echo "$ respuesta" | grep -iq "^ y"; luego
 
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-echo -n "Ingrese la IP: "
-read my_var
-    ping -c10 -i3 $my_var
-else
-    clear
+if [ " $ respuesta "  ! =  " $ {respuesta # [Yy]} " ] ; entonces
+echo -n " Ingrese la IP: "
+leer my_var
+    ping -c10 -i3 $ my_var
+más
+    claro
 fi
