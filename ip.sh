39cf25a0b01a13ca460aa50bc3d207bbc3e7b645
luis2046200 <62407028+luis2046200@users.noreply.github.com>
2020-12-08 21:10:21 -0500

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-ping -c10 -i3 192.168.1.1
+echo -n "Desea saber su Ping (y/n)? "
+read answer
 
+# if echo "$answer" | grep -iq "^y" ;then
 
-
-
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    ping -c10 -i3 192.168.1.1
+else
+    clear
+fi
