d9bf184b7a6c1ef95dab0bed242141409fdb8ceb
luis2046200 <62407028+luis2046200@users.noreply.github.com>
2020-12-08 21:36:34 -0500

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#Pregunta si o no
 echo -n "Desea saber su Ping (y/n)? "
 read answer
-
+clear
+#Buscador del ip local
+printf "Tu IP:"
+hostname -I | cut -d' ' -f1 
 # if echo "$answer" | grep -iq "^y" ;then
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-echo -n "Ingrese el ip: "
+echo -n "Ingrese la IP: "
 read my_var
     ping -c10 -i3 $my_var
 else
